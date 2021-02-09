b09cd20a96af76464d2dbaab051e486f9c270b36
Martin Danek <martindanek4@gmail.com>
2021-02-09 16:28:52 +0100

    main

--- chamber_of_deputies.py
+++ chamber_of_deputies.py
@@ -5,4 +5,11 @@ import election_year
 
 
 def chamber_of_deputies(url, file_name):
-    year = election_year(url)
+    year = election_year.election_year(url)
+
+    if year <= 1998:
+        election_scraper_1996.chamber_of_deputies()
+    elif year == 2002:
+        election_scrape_2002.chamber_of_deputies()
+    else:
+        election_scraper_2006.chamber_of_deputies()
